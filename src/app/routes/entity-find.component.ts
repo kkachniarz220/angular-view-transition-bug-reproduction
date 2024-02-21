b24df5c4537638ade5b990d930be48e67d0c5590
Krzysztof <kkachniarz@betasi.pl>
2024-02-21 15:53:26 +0100

    refactor

--- src/app/routes/entity-find.component.ts
+++ src/app/routes/entity-find.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject, viewChild} from '@angular/core';
 import {FormsModule, NgForm} from "@angular/forms";
 import {Router} from "@angular/router";
-import {EntityService} from "../data-access/entity/entity.service";
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-entity-find',
@@ -21,7 +21,6 @@ import {EntityService} from "../data-access/entity/entity.service";
 export class EntityFindComponent {
 
   private _router = inject(Router);
-  private _entityService = inject(EntityService);
   ngForm = viewChild.required(NgForm);
 
   id: string = '';
@@ -29,11 +28,15 @@ export class EntityFindComponent {
   search() {
     console.log('form submitted');
     const id = this.ngForm().value.id;
-    this._entityService.getEntity(id)
+    this.getEntity(id)
       .subscribe(({id}) => {
         console.log('entity details fetched, navigation start');
         this._router.navigate(['entity', id])
           .then(() => console.log('nav end'))
       })
+  }
+
+  private getEntity(id: string) {
+    return of({id});
   }
 }
